f5a28ef77f013dbbe591ff526aa087c0737ff351
MedAmineMod <soufianesbaa377@gmail.com>
2019-03-27 17:17:42 +0100

    2sides app js

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','app.side1_app','app.side2_app'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -317,23 +317,3 @@ $urlRouterProvider.otherwise('/tab/home');})
  // helloooooooooo
  // branch
 }])
-
-.controller('aboutCtrl', function($scope, $state, $http) {
-  // Form data for the login modal
-  $scope.loginData = {};
-   // Perform the login action when the user submits the login for
-  $scope.doLogin = function() {
-    //console.log($scope.loginData);
-    $http.post("http://samira_food.wcode-agency.com/test_login",($scope.loginData),{headers: {'Content-Type': 'multipart/form-data'}})
-      .success(function(data){
-        console.log(data);
-      });
-    };
-  $scope.doLogin = function() {
-      //console.log($scope.loginData);
-  $http.post("http://samira_food.wcode-agency.com/test_login",($scope.loginData),{headers: {'Content-Type': 'multipart/form-data'}})
-    .success(function(data){
-      console.log(data);
-    });
-  };
-})
