3c57b0084be189406cab6dd81d11ef4bf08eb324
MedAmineMod <soufianesbaa377@gmail.com>
2019-04-16 11:44:21 +0100

    fuzion

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,31 @@
-angular.module('starter', ['ionic','app.side1_app','app.side2_app'])
+angular.module('starter', ['ionic','app.side2_app'])
+
+.filter("FilterbyAll", function(){
+
+  return function(users, postDate){
+
+       var addUser;
+       var selectedUsers = [];
+       console.log(postDate);
+       if (postDate == 0) {
+         postDate = 2000;
+       }
+       for(i=0;i<users.length;i++){
+           addUser = false;
+           if (parseInt(users[i].totalminutes) <= postDate){
+             console.log(users[i].totalminutes);
+                addUser = true;
+               if (addUser){
+                   selectedUsers.push(users[i]);
+               }
+
+           }
+
+   }
+       return selectedUsers;
+   };
+
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,8 +43,8 @@ var admobid = {};
 // TODO: replace the following ad units with your own
 if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
-    banner: 'AD_UNIT_ID_BANNER',
-    interstitial: 'AD_UNIT_ID_INTERSTICIAL'
+    banner: 'ca-app-pub-3940256099942544/6300978111',
+    interstitial: 'ca-app-pub-3940256099942544/1033173712'
   };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
@@ -247,11 +274,18 @@ $urlRouterProvider.otherwise('/tab/home');})
 //END OF Category and categories Controllers :
 //***************************************************************************************
 //Videos-recipes  and videos Controllers :
-.controller('VideosCtrl' ,['$scope', '$http', '$state', function($scope, $http, $state) {
-  $http.get('http://api.wcode-agency.com/videos.json')
+.controller('VideosCtrl' ,['$scope', '$http', '$state','$sce' ,function($scope, $http, $state ,$sce) {
+  $http.get('http://samira_food.wcode-agency.com/json_recipes.json')
   .success(function(data){
-    $scope.videos = data.videos;
-    console.log(data.videos);
+    $scope.movie  ;
+    $scope.recipes = data.recipes ;
+    // for (var i = 0; i < $scope.recipes.length; i++) {
+        $scope.trustSrc = function(src) {
+       return $sce.trustAsResourceUrl(src);
+        }
+        // $scope.movie[i]   = {src:""+ data.recipes[i].normal_video_link +"", title:"tata"};
+    // }
+
   });
 }] )
 .controller('VideoCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
@@ -280,7 +314,6 @@ $urlRouterProvider.otherwise('/tab/home');})
 }])
 
 .controller('ChefCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
-
   $scope.params = $state.params ;
   $http.get('http://samira_food.wcode-agency.com/json_recipes.json')
   .success(function(data){
@@ -297,18 +330,25 @@ $urlRouterProvider.otherwise('/tab/home');})
 
 }])
 
-.controller('DetailsCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
+.controller('DetailsCtrl', ['$scope', '$http', '$state' ,  '$sce', function($scope, $http, $state , $sce) {
   $http.get('http://samira_food.wcode-agency.com/json_recipes.json')
   .success(function(data){
 
     for (var i = 0; i < data.recipes.length; i++) {
     console.log(  data.recipes[i].id);
+
     if ($state.params.id == data.recipes[i].id ) {
         $scope.data = data.recipes[i];
+        $scope.trustSrc = function(src) {
+
+       return $sce.trustAsResourceUrl(src);
+        }
+        $scope.movie = {src:""+ data.recipes[i].video_link +"", title:"Egghead.io AngularJS Binding"};
     }
     }
-    console.log(data.recipes);
+    // console.log(data.recipes);
     $scope.recipes = data.recipes;
+    //video in detailsComponent
   });
 
   $http.get('http://samira_food.wcode-agency.com/json_chefs.json')
@@ -318,7 +358,7 @@ $urlRouterProvider.otherwise('/tab/home');})
 
   }])
 
-  .controller('SearchCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
+  .controller('SearchCtrl', ['$scope', '$http' , '$state', function($scope, $http, $state) {
   $http.get('http://samira_food.wcode-agency.com/json_recipes.json')
   .success(function(data){
     $scope.data = data.recipes[$state.params.id];
@@ -329,6 +369,198 @@ $urlRouterProvider.otherwise('/tab/home');})
   .success(function(data){
     $scope.chefs = data.chefs;
   });
- // helloooooooooo
+
+
+  // FILTER BY
+
+    $scope.groups = [];
+    $scope.btnVal = "" ;
+    $scope.serveTime  =  "" ;
+    $scope.cato = "" ;
+    var btns  = document.getElementsByClassName("btn-diff");
+    // console.log(btns);
+
+    $scope.orderByMe = function(x) {
+        if (x == "EASY" || x =="HARD" || x == "MASTER" || x == '') {
+          $scope.btnVal = x ;
+          console.log($scope.btnVal);
+        };
+        if (x == 60 || x ==  15  || x == 30) {
+          $scope.serveTime = x ;
+          console.log($scope.serveTime);
+        };
+  if (x == 17 || x ==  18  || x == 19) {
+            $scope.cato = x ;
+          console.log($scope.cato);
+        };
+    };
+
+    $scope.replaceOther = function (index) {
+
+        if (index == 0) {
+            btns[1].classList.add(  'button-outline') ;
+            btns[2].classList.add(  'button-outline') ;
+            btns[3].classList.add( 'button-outline') ;
+
+        }else if(index == 1) {
+              btns[0].classList.add( 'button-outline') ;
+              btns[2].classList.add( 'button-outline') ;
+              btns[3].classList.add( 'button-outline') ;
+
+        }else if (index == 2) {
+              btns[0].classList.add( 'button-outline') ;
+              btns[1].classList.add( 'button-outline') ;
+              btns[3].classList.add( 'button-outline') ;
+
+        }else if (index == 3) {
+              btns[0].classList.add( 'button-outline') ;
+              btns[1].classList.add( 'button-outline') ;
+              btns[2].classList.add( 'button-outline') ;
+        }
+    };
+    $scope.replaceOtherTime = function (index) {
+
+        if (index == 4) {
+            btns[5].classList.add(  'button-outline') ;
+            btns[6].classList.add(  'button-outline') ;
+            btns[7].classList.add( 'button-outline') ;
+
+        }else if(index == 5) {
+              btns[6].classList.add( 'button-outline') ;
+              btns[4].classList.add( 'button-outline') ;
+              btns[7].classList.add( 'button-outline') ;
+
+        }else if (index == 6) {
+              btns[4].classList.add( 'button-outline') ;
+              btns[5].classList.add( 'button-outline') ;
+              btns[7].classList.add( 'button-outline') ;
+        }else  if (index == 7) {
+              btns[4].classList.add(  'button-outline') ;
+              btns[5].classList.add(  'button-outline') ;
+              btns[6].classList.add(  'button-outline') ;
+          }
+  //categories filter >>>
+        if (index == 8) {
+            btns[9].classList.add(  'button-outline') ;
+            btns[10].classList.add(  'button-outline') ;
+        }else if(index ==9) {
+              btns[8].classList.add( 'button-outline') ;
+              btns[10].classList.add( 'button-outline') ;
+        }else if (index ==10) {
+              btns[8].classList.add( 'button-outline') ;
+              btns[9].classList.add( 'button-outline') ;
+        }
+    };
+
+    $scope.makeActive = function( ) {
+      if (btns[0].classList.contains('button-outline')) {
+            btns[0].classList.remove('button-outline') ;
+              $scope.replaceOther (0) ;
+          }
+
+    };
+    $scope.makeActiveHard = function() {
+      if (btns[1].classList.contains('button-outline')) {
+          btns[1].classList.remove('button-outline') ;
+        }
+        $scope.replaceOther (1) ;
+        };
+
+    $scope.makeActiveMaster = function() {
+      if (btns[2].classList.contains('button-outline')) {
+          btns[2].classList.remove('button-outline') ;
+    }
+         $scope.replaceOther (2) ; };
+    $scope.makeActiveNone = function() {
+      if (btns[3].classList.contains('button-outline')) {
+          btns[3].classList.remove('button-outline') ;
+    }
+         $scope.replaceOther (3) ; };
+
+
+    $scope.makeActiveTimeOne  = function() {
+      if (btns[4].classList.contains('button-outline')) {
+          btns[4].classList.remove('button-outline') ;
+            $scope.replaceOtherTime(4) ;
+      }
+    };
+    $scope.makeActiveTimeTow  = function() {
+      if (btns[5].classList.contains('button-outline')) {
+          btns[5].classList.remove('button-outline') ;
+            $scope.replaceOtherTime(5) ;
+      }
+    };
+    $scope.makeActiveTimeThree  = function() {
+      if (btns[6].classList.contains('button-outline')) {
+          btns[6].classList.remove('button-outline') ;
+            $scope.replaceOtherTime(6) ;
+      }
+    };
+    $scope.makeActiveTimeThreePlus  = function() {
+      if (btns[7].classList.contains('button-outline')) {
+          btns[7].classList.remove('button-outline') ;
+            $scope.replaceOtherTime(7) ;
+      }
+    };
+
+    $scope.makeActiveCatOne  = function() {
+      if (btns[8].classList.contains('button-outline')) {
+          btns[8].classList.remove('button-outline') ;
+            $scope.replaceOtherTime(8) ;
+      }
+    };
+    $scope.makeActiveCatTow  = function() {
+      if (btns[9].classList.contains('button-outline')) {
+          btns[9].classList.remove('button-outline') ;
+            $scope.replaceOtherTime(9) ;
+      }
+    };
+    $scope.makeActiveCatThree  = function() {
+      if (btns[10].classList.contains('button-outline')) {
+          btns[10].classList.remove('button-outline') ;
+            $scope.replaceOtherTime(10) ;
+      }
+    };
+
+
+
+
+    for (var i=0; i<1; i++) {
+      $scope.groups[i] = {
+        name: "",
+        items: [],
+        show: false
+      };
+
+      for (var j=0; j<1; j++) {
+         $scope.groups[i].items.push(i + '-' + j);
+       }
+
+  };
+
+
+
+  $scope.toggleGroup = function(group) {
+    group.show = !group.show;
+  };
+  $scope.isGroupShown = function(group) {
+    return group.show;
+  };
+
+
+
  // branch
 }])
+// .filter('filterByTime' ,  function () {
+//   return function (times ,val) {
+//   var filtered = [];
+//   for (var i = 0; i < times.length; i++) {
+//   var item = times[i];
+//   if (item.totalminutes  < val  ) {
+//     filtered.push(item);
+//   }
+//   }
+// return filtered;
+// };
+//
+// });
--- www/js/side2_app.js
+++ www/js/side2_app.js
@@ -1,20 +1,5 @@
 angular.module('app.side2_app', ['ionic'])
-.controller('aasdfasdf', function($scope, $state, $http) {
-  // Form data for the login modal
-  $scope.loginData = {};
-   // Perform the login action when the user submits the login for
-  $scope.doLogin = function() {
-    //console.log($scope.loginData);
-    $http.post("http://samira_food.wcode-agency.com/test_login",($scope.loginData),{headers: {'Content-Type': 'multipart/form-data'}})
-      .success(function(data){
-        console.log(data);
-      });
-    };
-  $scope.doLogin = function() {
-      //console.log($scope.loginData);
-  $http.post("http://samira_food.wcode-agency.com/test_login",($scope.loginData),{headers: {'Content-Type': 'multipart/form-data'}})
-    .success(function(data){
-      console.log(data);
-    });
-  };
+
+.controller('FilterCtrl', function($scope) {
+
 })
